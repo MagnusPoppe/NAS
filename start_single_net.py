3e10430890b168f05aa5310254ba807124faee3b
Magnus Poppe Wang <Magnus.poppe.wang@gmail.com>
2019-05-01 16:43:17 +0200

    Separating out single-net into its own runnable

---
+++ start_single_net.py
@@ -0,0 +1,44 @@
+import os
+import sys
+
+from src.configuration import Configuration
+from src.output import print_config_stats
+
+try:
+    import setproctitle
+    setproctitle.setproctitle("NAS-EVOLVE")
+except ImportError: pass
+from firebase.upload import create_new_run, update_run
+
+import src.single_net.main as ea_nas
+
+if __name__ == '__main__':
+    # Reading input arguments:
+    if len(sys.argv) > 2:
+        raise IOError("Program requires dataset config file.")
+    if not os.path.isfile(sys.argv[1]):
+        raise IOError("File {} does not exist!".format(sys.argv[1]))
+
+    # Setting up config:
+    config = Configuration.from_json(sys.argv[1])
+    config.MPI = True
+    config.type = "ea-nas"
+    run_id = create_new_run(config)
+    if run_id:
+        config.results_name = run_id
+    print_config_stats(config)
+
+    # Running the algorithm:
+    status = "Running"
+    try:
+        ea_nas.run(config)
+        status = "Finished"
+    except KeyboardInterrupt:
+        status = "Closed"
+    except Exception as e:
+        status = "Crashed"
+        print("\nException caught in outermost loop:")
+        print(e)
+        raise e
+    finally:
+        update_run(config, status)
